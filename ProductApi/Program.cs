08da38e20321a808265e04d7460d9cedd819e869
Web Development <rmfredm@stu.ccsdut.org>
2026-06-23 10:49:20 -0600

    Populate index.html with products from database
    index.js now correctly pulls data from the database and populates the products-list ul on index.html with the products. Program.cs accommodates this by allowing the database to be accessed by the website's origin and populating the database with two entries if it is empty.

--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -1,24 +1,43 @@
 /*
 Author: Joshua Willis
 Created: 6/22/2026
-Updated: 6/22/2026
+Updated: 6/23/2026
 Create and run the product database for the e-commerce site 
 */
 // import namespaces
 using Microsoft.EntityFrameworkCore;
 
-// build the webapp for the database
+// begin building the webapp for the database
 var builder = WebApplication.CreateBuilder(args);
+
+// allow the frontend to access the database
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  => {
+                          policy.WithOrigins("http://localhost:5173");
+                      });
+});
+
+// add database context
 builder.Services.AddDbContext<ProductDb>(opt => opt.UseInMemoryDatabase("ProductList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// finish building the database webapp
 var app = builder.Build();
+app.UseCors(MyAllowSpecificOrigins);
 
 // handle a Get request by asynchronously returning a list of the Products in the database
 app.MapGet("/products", async (ProductDb db) => {
-        db.Products.Add(new Product("Apple", "A red fruit", 2.99));
-        db.Products.Add(new Product("Banana", "A yellow fruit", 1.99));
-        await db.Products.ToListAsync();
-    });
+    // seed the database if it is empty
+    if (db.Products.Count() == 0) {
+        db.Products.Add(new Product { Name = "Apple", Description = "a red fruit", Price = 2.99 });
+        db.Products.Add(new Product { Name = "Banana", Description = "a yellow fruit", Price = 1.99 });
+        await db.SaveChangesAsync();
+    }
+    return await db.Products.ToListAsync();
+});
 
 // run the database
 app.Run();
