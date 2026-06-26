4758476e4b5f2187e5c9bc9b857bb79341ef94bc
Web Development <rmfredm@stu.ccsdut.org>
2026-06-26 13:26:57 -0600

    Create e2e tests for T18-5
    spec.cs now tests for a form that creates new products

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,4 +1,8 @@
+// import faker
+import { faker } from "@faker-js/faker";
+
 describe("Products Page", () => {
+	// test that products are listed
 	it("lists products", () => {
 		// visit the products page
 		cy.visit("http://localhost:5173");
@@ -8,5 +12,57 @@ describe("Products Page", () => {
 
 		// ensure there is an unordered list of products
 		cy.get('ul[name="products-list"]').should("be.visible");
+	});
+
+	// test that products can be created
+	it("creates products", () => {
+		// visit the products page
+		cy.visit("http://localhost:5173");
+
+		// generate fake data using faker
+		const randomName = faker.commerce.product();
+		const randomPrice = faker.commerce.price();
+		const randomCount = faker.number.int();
+
+		// ensure there is a form with inputs for product name, price, and inventory count and input the data
+		cy.get('form[name="create-product-form"]').should("be.visible");
+
+		cy.get(
+			'form[name="create-product-form"] > input[name="name"][type="text"]',
+		)
+			.should("be.visible")
+			.type(randomName);
+
+		cy.get(
+			'form[name="create-product-form"] > input[name="price"][type="number"]',
+		)
+			.should("be.visible")
+			.type(randomPrice);
+
+		cy.get(
+			'form[name="create-product-form"] > input[name="inventory count"][type="number"]',
+		)
+			.should("be.visible")
+			.type(randomCount);
+
+		cy.get('form[name="create-product-form"] > button[type="submit"]')
+			.should("be.visible")
+			.and("have.text", "Create Product")
+			.click();
+
+		cy.url().should("eq", "/");
+
+		// check that the new product was added to the product list
+		cy.get('ul[name="products-list"] li:last')
+			.should("be.visible")
+			.and(
+				"have.text",
+				randomName +
+					": $" +
+					randomPrice +
+					", " +
+					randomCount +
+					" in inventory",
+			);
 	});
 });
