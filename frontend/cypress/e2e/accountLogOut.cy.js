c28daccf269a29cd506251a4fa3b35afdb0525b9
Web Development <rmfredm@stu.ccsdut.org>
2026-07-07 10:28:49 -0600

    Create e2e test suite for T18-12
    Cypress now tests for a logout button that logs the user out of their account.

---
+++ frontend/cypress/e2e/accountLogOut.cy.js
@@ -0,0 +1,28 @@
+// import faker
+import { faker } from "@faker-js/faker";
+
+describe("Log Out Process", () => {
+	it("logs out of accounts", () => {
+		// create account to log out of and log into it
+		// create an account to log into
+		const email = faker.internet.email();
+		const password = faker.internet.password();
+		cy.createAccount(email, password);
+		cy.logIn(email, password);
+
+		// visit the homepage
+		cy.visit(Cypress.config().baseUrl);
+
+		// ensure the logout button is visible and click on it
+		cy.get('button[id="logout-button"]')
+			.should("be.visible")
+			.and("have.text", "Log Out")
+			.click();
+
+		// ensure a logout success message is displayed
+		cy.url().should("eq", Cypress.config().baseUrl);
+		cy.get('p[id="message"]')
+			.should("be.visible")
+			.and("contain.text", "Logged out!");
+	});
+});
